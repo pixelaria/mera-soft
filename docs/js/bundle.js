c0faead48183dfba1187bbec6bacb2af3d123e92
Nikita <nikita.kruchinkin@gmail.com>
2018-02-06 01:22:35 +0300

    A lot of new sections

---
+++ docs/js/bundle.js
@@ -0,0 +1,2 @@
+
+$(function(){console.log('init');$('.radioblock__item').click(function(e){$(this).parent().find('.radioblock__item').removeClass('radioblock__item--active');$(this).addClass('radioblock__item--active');});});
