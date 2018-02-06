2b421df65bddc7f69bd50fbaad1d07f26008cc6e
Pixelaria <info@pixelaria.ru>
2018-02-06 16:39:10 +0300

    Footer, form section, map section, abour section/

--- docs/js/bundle.js
+++ docs/js/bundle.js
@@ -1,2 +1,3 @@
 
+function initMap(){var uluru={lat:55.709759,lng:37.597026};var map=new google.maps.Map(document.getElementById('map'),{zoom:17,center:uluru});var marker=new google.maps.Marker({position:uluru,map:map});}
 $(function(){console.log('init');$('.radioblock__item').click(function(e){$(this).parent().find('.radioblock__item').removeClass('radioblock__item--active');$(this).addClass('radioblock__item--active');});});
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,15 @@
+function initMap() {
+  var uluru = {lat: 55.709759, lng: 37.597026};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+
 $(function (){
   console.log('init');
 
